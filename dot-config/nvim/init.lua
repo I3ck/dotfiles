7b8cbb5aaf4b59327bbf59904e0c4fefa9e55036
Martin Buck <buckmartin@buckmartin.de>
2025-12-03 10:04:58 +0100

    add nvim telescope plugin

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -2,3 +2,13 @@ vim.cmd("colorscheme retrobox")
 
 vim.cmd("set number")
 vim.cmd("set autoindent")
+
+-- Set space to be <leader> key
+vim.cmd("nnoremap <SPACE> <Nop>")
+vim.cmd("let mapleader=\" \"")
+
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>p', builtin.find_files, {})
+vim.keymap.set('n', '<leader>f', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>b', builtin.buffers, {})
