cb825b2847edf7e59da5e3cc80f815470d29af32
Martin Buck <buckmartin@buckmartin.de>
2025-12-03 21:24:13 +0100

    nvim use more space again

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -5,19 +5,24 @@ vim.cmd("set autoindent")
 vim.cmd("set splitright")
 vim.cmd("set splitbelow")
 
+-- Set space to be <leader> key
+vim.cmd("nnoremap <space> <nop>")
+vim.cmd("let mapleader=\" \"")
+
 -- Single key window traversal
-vim.keymap.set('n', '<C-J>', '<C-W><C-J>', {noremap = true})
-vim.keymap.set('n', '<C-K>', '<C-W><C-K>', {noremap = true})
-vim.keymap.set('n', '<C-L>', '<C-W><C-L>', {noremap = true})
-vim.keymap.set('n', '<C-H>', '<C-W><C-H>', {noremap = true})
-vim.keymap.set('n', '<C-S>', ':vs<cr>', {noremap = true})
+vim.keymap.set('n', '<leader>j', '<C-W><C-J>', {noremap = true})
+vim.keymap.set('n', '<leader>k', '<C-W><C-K>', {noremap = true})
+vim.keymap.set('n', '<leader>l', '<C-W><C-L>', {noremap = true})
+vim.keymap.set('n', '<leader>h', '<C-W><C-H>', {noremap = true})
+vim.keymap.set('n', '<leader>s', ':vs<cr>', {noremap = true})
 
 vim.lsp.enable('rust_analyzer')
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-P>', builtin.find_files, {})
-vim.keymap.set('n', '<C-F>', builtin.live_grep, {})
-vim.keymap.set('n', '<C-B>', builtin.buffers, {})
+vim.keymap.set('n', '<leader>p', builtin.find_files, {})
+vim.keymap.set('n', '<leader>f', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+vim.keymap.set('n', '<leader>d', builtin.diagnostics, {})
 
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('my.lsp', {}),
