b59c0ab81050c16b02aa56da56e1fed6bba72cb9
Martin Buck <buckmartin@buckmartin.de>
2025-12-03 15:52:29 +0100

    improve nvim further

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -2,14 +2,19 @@ vim.cmd("colorscheme retrobox")
 
 vim.cmd("set number")
 vim.cmd("set autoindent")
+vim.cmd("set splitright")
+vim.cmd("set splitbelow")
 
--- Set space to be <leader> key
-vim.cmd("nnoremap <SPACE> <Nop>")
-vim.cmd("let mapleader=\" \"")
+-- Single key window traversal
+vim.keymap.set('n', '<C-J>', '<C-W><C-J>', {noremap = true})
+vim.keymap.set('n', '<C-K>', '<C-W><C-K>', {noremap = true})
+vim.keymap.set('n', '<C-L>', '<C-W><C-L>', {noremap = true})
+vim.keymap.set('n', '<C-H>', '<C-W><C-H>', {noremap = true})
+vim.keymap.set('n', '<C-S>', ':vs<cr>', {noremap = true})
 
 vim.lsp.enable('rust_analyzer')
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>p', builtin.find_files, {})
-vim.keymap.set('n', '<leader>f', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+vim.keymap.set('n', '<C-P>', builtin.find_files, {})
+vim.keymap.set('n', '<C-F>', builtin.live_grep, {})
+vim.keymap.set('n', '<C-B>', builtin.buffers, {})
