cb0ed1c06cbd9316c35433ed99b6dcebd5793cf2
Martin Buck <buckmartin@buckmartin.de>
2025-12-13 22:38:42 +0100

    elixir-ls for helix and nvim

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -26,6 +26,11 @@ vim.lsp.config('rust_analyzer', {
     }
 })
 
+vim.lsp.enable('elixirls')
+vim.lsp.config('elixirls', {
+    cmd = { "elixir-ls" };
+})
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<c-p>', builtin.find_files, {})
 vim.keymap.set('n', '<c-f>', builtin.live_grep, {})
